08e6ddd25615c43f06da5b35d9daa2348f8d6e47
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-09-08 12:02:00 -0400

    button

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ let char = ["Gandalf", "Jack Ryan", "Thor", "Harry Potter"]
 let noun2 = ["issues", "wrath", "unending joy", "frustration"]
 
 
-var nounResult = noun[Math.floor(Math.random() * noun.length)];
-var adjResult = adj[Math.floor(Math.random() * adj.length)];
-var charResult = char[Math.floor(Math.random() * char.length)];
-var noun2Result = noun2[Math.floor(Math.random() * noun2.length)];
+// var nounResult = noun[Math.floor(Math.random() * noun.length)];
+// var adjResult = adj[Math.floor(Math.random() * adj.length)];
+// var charResult = char[Math.floor(Math.random() * char.length)];
+// var noun2Result = noun2[Math.floor(Math.random() * noun2.length)];
 
 
-console.log(nounResult)
+// console.log(nounResult)
+function promtFunction() {
+    var nounResult = noun[Math.floor(Math.random() * noun.length)];
+    var adjResult = adj[Math.floor(Math.random() * adj.length)];
+    var charResult = char[Math.floor(Math.random() * char.length)];
+    var noun2Result = noun2[Math.floor(Math.random() * noun2.length)];
 
-$("#prompt").append("The " + adjResult + " " + nounResult + " is dealing with the " + noun2Result + " of " + charResult)
+
+$("#prompt").html("The " + adjResult + " " + nounResult + " is dealing with the " + noun2Result + " of " + charResult)
+}
