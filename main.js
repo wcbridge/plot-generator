eee543a8a0c2de3988df1d7b60422dfa64a570ae
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-11-07 14:20:15 -0500

    fixed undefined in adj array

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let noun = ["house", "truck", "castle", "beer", "area", "book", "business", "case", "child", "company","country","day","eye","fact","family","government","group","hand","home","job","life","lot","man","money","month","mother","night","number","part","people","place","point","problem","program","question","right","room","school","state","story","student","study","system","thing","time","water","way","week","woman","word","work","world","year"]
-let adj = ["big", "short", "incrdible", "", , "", "bad","best","better","big","black","certain","clear","different","early","easy","economic","federal", "free","full","good","great","hard","high","human","important","international","large","late","little","local","long","low","major","military","national","new", "old","only","other","political","possible","public","real","recent","right","small","social","special","strong","sure","true","white","whole","young"]
+let adj = ["big", "short", "incrdible", "", "", "bad","best","better","big","black","certain","clear","different","early","easy","economic","federal", "free","full","good","great","hard","high","human","important","international","large","late","little","local","long","low","major","military","national","new", "old","only","other","political","possible","public","real","recent","right","small","social","special","strong","sure","true","white","whole","young"]
 let char = ["Gandalf", "Jack Ryan", "Thor", "Harry Potter", "the hero", "the protagontist", "the antagonist", "the villian", "the wiazrd", "the sugar plum fairy" ]
-let noun2 = ["issues", "wrath", "unending joy", "frustration"]
+let noun2 = ["issues", "wrath", "unending joy", "frustration", ]
 
 
 // var nounResult = noun[Math.floor(Math.random() * noun.length)];
