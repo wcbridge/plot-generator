2e44fea967f68843a2708ae3d66c640e62f536ab
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-11-16 09:39:53 -0500

    added to char array

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let noun = ["house", "truck", "castle", "beer", "area", "book", "business", "case", "child", "company","country","day","eye","fact","family","government","group","hand","home","job","life","lot","man","money","month","mother","night","number","part","people","place","point","problem","program","question","right","room","school","state","story","student","study","system","thing","time","water","way","week","woman","word","work","world","year"]
 let adj = ["big", "short", "incrdible", "", "", "bad","best","better","big","black","certain","clear","different","early","easy","economic","federal", "free","full","good","great","hard","high","human","important","international","large","late","little","local","long","low","major","military","national","new", "old","only","other","political","possible","public","real","recent","right","small","social","special","strong","sure","true","white","whole","young"]
-let char = ["Gandalf", "Jack Ryan", "Thor", "Harry Potter", "the hero", "the protagontist", "the antagonist", "the villian", "the wiazrd", "the sugar plum fairy" ]
+let char = ["Gandalf", "Jack Ryan", "Thor", "Harry Potter", "the hero", "the protagontist", "the antagonist", "the villian", "the wiazrd", "the sugar plum fairy", "the architect"]
 let noun2 = ["issues", "wrath", "unending joy", "frustration", ]
 
 
@@ -12,10 +12,10 @@ let noun2 = ["issues", "wrath", "unending joy", "frustration", ]
 
 // console.log(nounResult)
 function promtFunction() {
-    var nounResult = noun[Math.floor(Math.random() * noun.length)];
-    var adjResult = adj[Math.floor(Math.random() * adj.length)];
-    var charResult = char[Math.floor(Math.random() * char.length)];
-    var noun2Result = noun2[Math.floor(Math.random() * noun2.length)];
+    let nounResult = noun[Math.floor(Math.random() * noun.length)];
+    let adjResult = adj[Math.floor(Math.random() * adj.length)];
+    let charResult = char[Math.floor(Math.random() * char.length)];
+    let noun2Result = noun2[Math.floor(Math.random() * noun2.length)];
 
 
 $("#prompt").html("The " + adjResult + " " + nounResult + " is dealing with the " + noun2Result + " of " + charResult)
